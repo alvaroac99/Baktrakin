25c45717c9e71ecd556a49a74c381c1613069e3e
alvaroac99 <vavohockey@gmail.com>
2020-11-28 20:10:27 +0100

    prueba1

--- src/Pruebas.java
+++ src/Pruebas.java
@@ -1,6 +1,14 @@
+import java.util.*;
+import java.lang.*;
+
 public class Pruebas {
 
     public static void main(){
-
+        System.out.println("***********************************************************");
+        System.out.println("*****************EJERCICIOS DE BACKTRACKING****************");
+        System.out.println("***********************************************************");
     }
+
 }
+
+
