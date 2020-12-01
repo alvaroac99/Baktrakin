c801b528b3626d4a2d3ea13dc8bf5f9f869d4fd0
alvaroac99 <vavohockey@gmail.com>
2020-12-01 18:37:01 +0100

    ejercico de arbol binario

--- src/Ejercicios.java
+++ src/Ejercicios.java
@@ -1,2 +1,51 @@
 public class Ejercicios {
+
+    /*ESQUEMA GENERAL
+
+    * void vueltaAtras (Estado estado, Booleano exito, …){
+            if (esSolucion(estado)) {
+                exito.setValor(true);
+            } else {
+                   inicializar_conjunto_de_candidatos();
+                   while ((!exito.getValor()) && (hay_mas_candidatos())){
+                        seleccionar_un_candidato();
+                        if (aceptable(candidato)){
+                             proximoEstado = anotar_candidato();
+                             vueltaAtras(proximoEstado, exito, ...);
+                             if (!exito.getValor()) {
+                                desanotar_candidato();
+                               }
+                        }if aceptable
+                    } while
+            } else
+    */
+
+    public static int suma (int[] solucion, int[] vector, int nivel){
+        int suma=0;
+        for (int i=0; i<nivel; i++)
+            if (solucion[i]==1) suma = suma+vector[i];
+        return suma;
+    }
+    //arbol de busqueda binario
+    public static void SumaSubconjuntos(int[] vector, int num, int[] sol, int nivel, Booleano exito) {
+        if (nivel == vector.length) {
+            if (suma(sol, vector, nivel )== num) exito.setValor(true);
+        } else {
+            int c = 0; //candidatos, si 0 no añadir 1 añadir
+            while (!exito.getValor() && (c < 2)) {
+                if (c == 0 || suma(sol, vector, nivel) + vector[nivel] <= num) {
+                    sol[nivel] = c;
+                    nivel = nivel + 1;
+                    SumaSubconjuntos(vector, num, sol, nivel, exito);
+                    if (!exito.getValor()) {
+                        nivel = nivel - 1;
+                        sol[nivel] = 0;
+                    }
+                }
+                c++;
+            }
+
+        }
+    }
 }
+
--- src/Pruebas.java
+++ src/Pruebas.java
@@ -3,10 +3,22 @@ import java.lang.*;
 
 public class Pruebas {
 
-    public static void main(){
+
+    private static int[] vector={11,7,13};
+    private static int num;
+    private static int[] sol;
+    private static int nivel;
+    private static Booleano exito;
+
+
+    public static void main(String[] args){
         System.out.println("***********************************************************");
         System.out.println("*****************EJERCICIOS DE BACKTRACKING****************");
         System.out.println("***********************************************************");
+        System.out.println("PRUEBA SUMA SUBCONJUNTOS CON ARBOL DE BUSQUEDA BINARIO");
+        Ejercicios.SumaSubconjuntos(vector,20,sol,3,exito);
+        for(int i=0;i<sol.length;i++)
+            System.out.println(sol[i]);
     }
 
 }
