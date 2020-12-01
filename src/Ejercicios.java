ac58e868ea07eb181e687689c25b8d029cc6d1cc
alvaroac99 <vavohockey@gmail.com>
2020-12-01 19:39:12 +0100

    Ejercicios T7 menos el ultimo
    Da error por la clase booleano ( quitada)

--- src/Ejercicios.java
+++ src/Ejercicios.java
@@ -20,16 +20,17 @@ public class Ejercicios {
             } else
     */
 
-    public static int suma (int[] solucion, int[] vector, int nivel){
-        int suma=0;
-        for (int i=0; i<nivel; i++)
-            if (solucion[i]==1) suma = suma+vector[i];
+    public static int suma(int[] solucion, int[] vector, int nivel) {
+        int suma = 0;
+        for (int i = 0; i < nivel; i++)
+            if (solucion[i] == 1) suma = suma + vector[i];
         return suma;
     }
+
     //arbol de busqueda binario
     public static void SumaSubconjuntos(int[] vector, int num, int[] sol, int nivel, Boolean exito) {
         if (nivel == vector.length) {
-            if (suma(sol, vector, nivel )== num) exito=true;
+            if (suma(sol, vector, nivel) == num) exito = true;
         } else {
             int c = 0; //candidatos, si 0 no añadir 1 añadir
             while (!exito && (c < 2)) {
@@ -45,6 +46,107 @@ public class Ejercicios {
                 c++;
             }
 
+        }
+    }
+
+    //BUSQUEDA BINARIA v2
+    public static void subconjuntoSumaBack2(int[] vector, int num, int[] solucion, int nivel, int suma,
+                                            Boolean exito) {
+        if (nivel == vector.length) {
+            if (suma == num) exito = true;
+        } else {
+            int c = 0;     // Candidatos: c=0 no añadir; c=1 añadir
+            while (!exito && (c < 2)) {
+                if (c == 0 || ((suma + vector[nivel]) <= num)) {
+                    solucion[nivel] = c;
+                    suma = suma + (vector[nivel] * c);
+                    nivel = nivel + 1;
+                    subconjuntoSumaBack2(vector, num, solucion, nivel, suma, exito);
+                    if (!exito) {
+                        nivel = nivel - 1;
+                        solucion[nivel] = 0;
+                        suma = suma - (vector[nivel] * c);
+                    }
+                }
+                c++;
+            }
+        }
+    }
+
+    //ARBOL DE ESTADOS COMBINATORIO
+    public static void SumaSubconjuntosCombinatorio(int[] vector, int num, int[] solucion, int id, int suma,
+                                                    Boolean exito) {
+        if (suma == num) exito = true;
+        else {
+            int c = id; // Candidatos: c = id..vector.length
+            while (!exito && (c < vector.length)) {
+                if ((suma + vector[c]) <= num) {
+                    solucion[c] = 1;
+                    suma = suma + (vector[c]);
+                    c = c + 1;
+                    SumaSubconjuntosCombinatorio(vector, num, solucion, c, suma, exito);
+                    if (!exito) {
+                        c = c - 1;
+                        solucion[c] = 0;
+                        suma = suma - (vector[c]);
+                    }
+                }
+                c++;
+            }
+        }
+    }
+
+    //PROBLEMA DEL CABALLERO
+
+
+    public static void Caballero ( int[][] tablero, int numMov, int x, int y, Boolean exito){
+        if(numMov>tablero.length*tablero.length) exito=true;
+        else{
+            int k, u, v;
+            int[] dx = {-1,-2,-2,-1, 1, 2, 2, 1};
+            int[] dy = {-2,-1, 1, 2, 2, 1,-1,-2};
+            k = 0;
+            while ((!exito) && (k < 8)){
+                u = x + dx[k];
+                v = y + dy[k];
+                if ((u >= 0) && (u < tablero.length) &&
+                        (v >= 0) && (v < tablero.length) &&
+                        (tablero[u][v] == 0) ) {
+                    tablero[u][v] = numMov;
+                    numMov++;
+                    Caballero(tablero, numMov, u, v, exito);
+                    if (!exito) {
+                        tablero[u][v] = 0;
+                        numMov--;
+                    }
+                }
+                k = k + 1;
+            }
+        }
+    }
+
+    //PROBLEMA DE LAS 8 REINAS
+    public static boolean aceptable(int[] damas, int fila, int c){
+        boolean correcto = true; int i=0;
+        while ((correcto) && (i<fila)) {
+            correcto = (damas[i] != c) && (Math.abs(damas[i]-c) != Math.abs(i-fila));
+            i++;
+        }
+        return correcto;
+    }
+    public static void reinas(int[] damas, int fila, Boolean exito){
+        if (fila==damas.length) exito = true;
+        else {
+            int c = 0;
+            while ((c < damas.length) && (!exito)) {
+                if (aceptable(damas, fila, c)){
+                    damas[fila] = c;
+                    fila=fila + 1;
+                    reinas(damas, fila, exito);
+                    if (!exito) fila = fila -1;
+                }
+                c++;
+            }
         }
     }
 }
--- src/Pruebas.java
+++ src/Pruebas.java
@@ -5,8 +5,9 @@ public class Pruebas {
 
 
     private static int[] vector={11,7,13};
-    private static int num;
-    private static int[] sol={20};
+    private static int num = 20;
+    private static int[] sol = new int[vector.length];
+    private static int suma = 0;
     private static int nivel;
     private static Boolean exito=false;
 
@@ -15,11 +16,36 @@ public class Pruebas {
         System.out.println("***********************************************************");
         System.out.println("*****************EJERCICIOS DE BACKTRACKING****************");
         System.out.println("***********************************************************");
-        System.out.println("PRUEBA SUMA SUBCONJUNTOS CON ARBOL DE BUSQUEDA BINARIO");
-        Ejercicios.SumaSubconjuntos(vector,20,sol,0,exito);
+        //System.out.println("PRUEBA SUMA SUBCONJUNTOS CON ARBOL DE BUSQUEDA BINARIO");        //NO FUNCIONA
+        //Ejercicios.SumaSubconjuntos(vector,20,sol,0,exito);
+        System.out.println("PRUEBA SUMA SUBCONJUNTOS CON ARBOL DE BUSQUEDA BINARIO V2");
+        for (int i=0; i<sol.length; i++) sol[i]=0;
+        Ejercicios.subconjuntoSumaBack2(vector, num, sol, 0, suma, exito);
+        for (int j=0; j<sol.length; j++) System.out.println(sol[j]);
+        for ( int j=0; j<sol.length; j++) System.out.println(vector[j]);
+        System.out.println(nivel+" "+suma+" "+exito );
 
-        for (int j=0;j<vector.length;j++)
-            System.out.println(vector[j]);
+
+        System.out.println("PROBLEMA DEL CABALLERO");
+        int[][] tablero = new int[6][6];
+        for (int i=0; i<tablero.length; i++)
+            for (int j=0; j< tablero.length; j++)
+                tablero[i][j]=0;
+        tablero[0][0]=1;
+
+        Ejercicios.Caballero(tablero, 2, 0, 0, exito);
+
+        for (int i=0; i<tablero.length; i++) {
+            for (int j = 0; j < tablero.length; j++) {
+                System.out.print(tablero[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
+        System.out.println("PROBLEMA DE LAS 8 REINAS");
+        int[] damas = new int[4];
+        Ejercicios.reinas(damas, 0, exito);
     }
 
 }
